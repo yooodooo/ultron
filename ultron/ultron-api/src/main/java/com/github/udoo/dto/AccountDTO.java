37bfc422385f639e51031cc336510dc40d94b87e
dong.yang <dong.yang@56qq.com>
2019-08-14 10:09:56 +0800

    change package

---
+++ ultron/ultron-api/src/main/java/com/github/udoo/dto/AccountDTO.java
@@ -0,0 +1,13 @@
+package com.github.udoo.dto;
+
+import lombok.Data;
+
+/**
+ * @author dong.yang
+ * @data 2019/8/13 16:19
+ */
+@Data
+public class AccountDTO {
+    private String code;
+    private String name;
+}
